803a110d4b60999a7adfb8c145e471bdd41125ba
uo269948 <71765996+uo269948@users.noreply.github.com>
2020-11-28 21:39:51 +0100

    Add files via upload

---
+++ Ejercicio-01/Cabecera.js
@@ -0,0 +1,9 @@
+//Información asignatura y estudiante
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2020-21";
+asignatura.estudiante = "Candela Bobes Junquera";
+asignatura.email = "uo269948@uniovi.es";
---
+++ Ejercicio-01/Titulo1.js
@@ -0,0 +1,4 @@
+//Titulo 1
+document.write("<h1>");
+document.write(asignatura.nombre);
+document.write("</h1>");
